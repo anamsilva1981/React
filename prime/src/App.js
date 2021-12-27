2abac9ac6df420cc61cc3bf713ed534fff60f2db
Ana Maria <ana.nikoly@gmail.com>
2021-12-27 07:18:27 -0300

    MyFirstApp

---
+++ prime/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'; // Importando react
+
+function App(){ // Criando função
+    return (
+        <h1>Olá Mundo</h1> //O que será apresentado na tela
+    );
+}
+
+export default App; // Exportar para Index.js
